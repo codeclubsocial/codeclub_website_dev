ed07d3701d2301a25afe8c1ea8df2f72c9ecbe82
Brian Jason <brianjason@gmail.com>
2017-08-13 22:58:27 -0700

    updated mongoose script

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// mongoose config needs to be updated according to:
+// https://github.com/mongolab/mongodb-driver-examples/blob/master/nodejs/mongooseSimpleExample.js
+
 var express = require("express");
 var mongoose = require("mongoose");
 var ejs = require("ejs");
@@ -6,99 +9,107 @@ var methodOverride = require("method-override");
 
 var app = express();
 
-// localhost or heroku?
-var deployment = 'heroku';
-
-if(deployment == 'heroku') {
-	mongoose.connect("mongodb://heroku_lqjlfs09:8gpk00scqsdp3k2an9oktsma76@ds035836.mlab.com:35836/heroku_lqjlfs09");
-} else {
-	mongoose.connect("mongodb://127.0.0.1/test_db");
-}
-
 //Setup
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
+// MONGODB URI: localhost or heroku?
 
-//Database Schema and conversion to Model
-var Schema = mongoose.Schema
-var msgSchema = new Schema ({
-	title: String,
-	body: String,
-	author: String, //should be fetched from user info after Authentication (Passport.js)
-	date: {type: Date, default: Date.now}
-});
-
-var msgBoard = mongoose.model("msgBoard", msgSchema);
+if(process.env.MONGODB_URI !== undefined) {
+	var uri = process.env.MONGODB_URI
+} else {
+	var uri = "mongodb://127.0.0.1/test_db"
+}
 
 
-//=========== Routes Below =============
+// var uri = process.env.MONGODB_URI
+// var uri = "mongodb://127.0.0.1/test_db"
 
+mongoose.Promise = global.Promise
+mongoose.connect(uri);
+var db = mongoose.connection;
 
-//Landing page - First page (root)
-app.get("/", function(req, res){
-	res.render("landing");
-});
+db.on('error', console.error.bind(console, 'connection error:'));
 
+db.once('open', function callback ()	{
+	//Database Schema and conversion to Model
+	var Schema = mongoose.Schema
 
-
-//Index Page - Showing all the posts
-app.get("/msgs", function(req, res){
-	msgBoard.find({}, function(err, msg){
-		if(err){
-			console.log(err)
-		} else {
-			res.render("index", {msg: msg});
-		}
+	var msgSchema = new Schema ({
+		title: String,
+		body: String,
+		author: String, //should be fetched from user info after Authentication (Passport.js)
+		date: {type: Date, default: Date.now}
 	});
-});
+
+	var msgBoard = mongoose.model("msgBoard", msgSchema);
+
+	//=========== Routes Below =============
 
 
-//New Route - Form/page where you create a new post
-app.get("/msgs/new", function(req, res){
-	res.render("new")
-});
-
-//Create(ing/ed) Route - The page the post has been created
-app.post("/msgs", function(req, res){
-//mongodb commands, reading the parsing data, redirecting to index page
-	msgBoard.create(req.body.msg, function(err, msg){
-		if(err){
-			console.log(err);
-			res.redirect("/msgs");
-		} else {
-			res.redirect("/msgs");
-		}
+	//Landing page - First page (root)
+	app.get("/", function(req, res){
+		res.render("landing");
 	});
-});
-
-//Show Route - Viewing the full message/page of the created post
-app.get("/msgs/:id", function(req, res){
-//mongodb commands, showing/finding msgs in detail (full)
-});
-
-//Edit Route - Editing the post
-app.get("/msgs/:id/edit", function(req, res){
-//mongodb commands,
-});
-
-//Update Route - Updating the post
-app.put("/msgs/:id", function(req, res){
-
-});
-
-//Delete Route - Deleting the post
-app.delete("/blogs/:id", function(req, res){
-
-});
 
 
 
+	//Index Page - Showing all the posts
+	app.get("/msgs", function(req, res){
+		msgBoard.find({}, function(err, msg){
+			if(err){
+				console.log(err)
+			} else {
+				res.render("index", {msg: msg});
+			}
+		});
+	});
 
 
-app.listen(3000, function(){
-	console.log("Starting Server on port 3000...");
-	console.log("Successfully connected to the Database");
+	//New Route - Form/page where you create a new post
+	app.get("/msgs/new", function(req, res){
+		res.render("new")
+	});
+
+	//Create(ing/ed) Route - The page the post has been created
+	app.post("/msgs", function(req, res){
+		//mongodb commands, reading the parsing data, redirecting to index page
+		msgBoard.create(req.body.msg, function(err, msg){
+			if(err){
+				console.log(err);
+				res.redirect("/msgs");
+			} else {
+				res.redirect("/msgs");
+			}
+		});
+	});
+
+	//Show Route - Viewing the full message/page of the created post
+	app.get("/msgs/:id", function(req, res){
+		//mongodb commands, showing/finding msgs in detail (full)
+	});
+
+	//Edit Route - Editing the post
+	app.get("/msgs/:id/edit", function(req, res){
+		//mongodb commands,
+	});
+
+	//Update Route - Updating the post
+	app.put("/msgs/:id", function(req, res){
+
+	});
+
+	//Delete Route - Deleting the post
+	app.delete("/blogs/:id", function(req, res){
+
+	});
+
+	app.listen(3000, function(){
+		console.log("\nStarting Server on port 3000...");
+		//console.log("Successfully connected to the Database");
+		console.log("MongoDB database => " + uri);
+	});
+
 });
