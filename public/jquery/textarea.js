91651e439dc2c047258debea846f9e471fdd13d1
zaki <zaki.sediqyar@gmail.com>
2017-08-20 16:56:38 -0700

    several changes made

---
+++ public/jquery/textarea.js
@@ -0,0 +1,8 @@
+jQuery.each(jQuery('textarea[data-autoresize]'), function() {
+    var offset = this.offsetHeight - this.clientHeight;
+
+    var resizeTextarea = function(el) {
+        jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+    };
+    jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+});
