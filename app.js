87a25c9f848d1b80dac4b366f51c849524496558
Craig Griffith <craig429@mac.com>
2017-08-25 17:35:28 +0000

    A trivial moficiation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
+// A trivial modification
+
 var app = express();
 var port = process.env.PORT || 3000;
 
