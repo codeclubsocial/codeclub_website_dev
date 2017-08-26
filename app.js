1d9fc7e912c5e56888de744339c98394aa3683c7
Craig Griffith <craig429@mac.com>
2017-08-26 15:01:16 +0000

    First real commit

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+var localDB = true;
 var express = require("express");
 var mongoose = require("mongoose");
 var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
-// A trivial modification
+var passport = require('passport')
+LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-//Database setup with mLab
-mongoose.connect(process.env.MONGODB_URI);
+if(localDB == true){
+    //Local Database 
+    mongoose.connect("mongodb://127.0.0.1/test_db");
+  }
+  else{
+    //Database setup with mLab
+    mongoose.connect(process.env.MONGODB_URI);
+  }
 
 //Setup
-// mongoose.connect("mongodb://127.0.0.1/test_db");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
-
-//Database Schema and conversion to Model
+//Message collection schema and conversion to Model
 var Schema = mongoose.Schema
 var msgSchema = new Schema ({
 	title: String,
@@ -31,6 +36,32 @@ var msgSchema = new Schema ({
 
 var msgBoard = mongoose.model("msgBoard", msgSchema);
 
+//Database collection and conversion to Model
+var userSchema = mongoose.Schema
+var userSchema = new Schema ({
+  username: String,
+  password: String,
+  extra: String,
+  date: {type: Date, default: Date.now}
+});
+
+var userData = mongoose.model("userData", userSchema);
+
+// Setup login for simple username/password auth
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    userData.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 //=========== Routes Below =============
 
@@ -65,9 +96,48 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+// Credentials check from login
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+// Validate login credentials
+app.get("/login/:id", function(req, res){
+  userData.findById(req.params.id, function(err, msg){
+    if(err){
+      console.log(err);
+      res.redirect("/forum");
+    } else {
+      res.render("show", {msg: msg});
+    }
+  });
+});
+
+
 //Sign Up Page
 app.get("/signup", function(req, res){
   res.render("signup");
+});
+
+//Post from Sign Up Page
+app.post("/signup", function(req, res){
+  console.log("signup post")
+  userData.create(req.body.msg, function(err, msg){
+    if(err){
+      console.log(err);
+      res.redirect("/index");
+    } else {
+      console.log("Name: ", req.body.msg.username);
+      console.log("Password: ", req.body.msg.password);
+//      userData.insertOne({
+//        username: req.body.msg.username,
+//        password: req.body.msg.password,
+//      });
+      res.redirect("/forum");
+    }
+  });
 });
 
 //Message Board Page - Showing all the posts
@@ -154,7 +224,8 @@ app.listen(port, function(){
 	console.log("\n" + "=".repeat(40));
 	console.log("*** INITIALIZATION ***");
 	console.log("-".repeat(40));
-	console.log("Starting Server on http://localhost:3000");
+  if(localDB == true) { console.log("Starting Server on http://localhost:3000"); }
+  else                { console.log("Starting Server on ", process.env.MONGODB_URI); }
 	console.log("Successfully connected to the Database");
 	console.log("=".repeat(40) + "\n");
 });
