212d90309c5ebdfa387116dd07e3a58a05593a85
Craig Griffith <craig429@mac.com>
2017-08-27 17:41:00 +0000

    Interim commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var localDB = true;
 var express = require("express");
+var session = require("express-session");
 var mongoose = require("mongoose");
 var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var nodemailer = require('nodemailer');
-var passport = require('passport')
-LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport'),LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -25,6 +25,28 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*
+// Experimental middleware calls
+app.use(function (req, res, next) {
+  console.log('App call @ ', Date.now())
+  next()
+})
+app.use('/forum', function (req, res, next) {
+  console.log('FRequest Type:', req.method)
+  next()
+})
+app.use('/about', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}, function (req, res, next) {
+  console.log('ARequest Type:', req.method)
+  next()
+})
+*/
 
 //Message collection schema and conversion to Model
 var Schema = mongoose.Schema
@@ -51,14 +73,18 @@ var userData = mongoose.model("userData", userSchema);
 // Setup login for simple username/password auth
 passport.use(new LocalStrategy(
   function(username, password, done) {
+    console.log("Marker 1")
     userData.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
+      if (err) { console.log("Marker 2"); return done(err); }
       if (!user) {
+        console.log("Marker 3");
         return done(null, false, { message: 'Incorrect username.' });
       }
       if (!user.validPassword(password)) {
+        console.log("Marker 4");
         return done(null, false, { message: 'Incorrect password.' });
       }
+      console.log("Marker 5");
       return done(null, user);
     });
   }
@@ -102,24 +128,28 @@ app.get("/login", function(req, res){
 });
 
 // Credentials check from login
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
+app.post("/login", function(req, res){
 
-// Validate login credentials
-app.get("/login/:id", function(req, res){
-  userData.findById(req.params.id, function(err, msg){
-    if(err){
-      console.log(err);
-      res.redirect("/forum");
-    } else {
-      res.render("show", {msg: msg});
+/*
+  userData.count({ username }, function(err, count){
+    if(err){ console.log(err);}
+    else{console.log('There are %d users registered.', count);}
+  });
+*/
+  userData.findOne({ username: req.body.username }, function(err, user){
+    if(err){ console.log(err);}
+    else{
+      if( !user ){
+        console.log( "Could not find user "+req.body.username + " in our database!" ); 
+        res.render("signup");
+      }
+      else{
+        console.log('Logging in: ', user.username)
+        res.redirect("/forum");
+      }
     }
   });
 });
-
 
 //Sign Up Page
 app.get("/signup", function(req, res){
@@ -128,18 +158,12 @@ app.get("/signup", function(req, res){
 
 //Post from Sign Up Page
 app.post("/signup", function(req, res){
-  console.log("signup post")
   userData.create(req.body.msg, function(err, msg){
     if(err){
       console.log(err);
       res.redirect("/index");
     } else {
-      console.log("Name: ", req.body.msg.username);
-      console.log("Password: ", req.body.msg.password);
-//      userData.insertOne({
-//        username: req.body.msg.username,
-//        password: req.body.msg.password,
-//      });
+      console.log("Created new user: ", req.body.msg.username);
       res.redirect("/forum");
     }
   });
