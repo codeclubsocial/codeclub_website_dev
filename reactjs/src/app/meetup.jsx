a18b3736a7c2ff4a8ce436e0973a2eecb38d5663
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-06 23:05:33 -0700

    started trying to get the meetup RSVP button to work

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -12,17 +12,40 @@ async function getMeetup() {
   }
 }
 
+async function postRSVP(rsvpData) {
+  try {
+    let response = await fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/codeclub/events/kzxzpnywmbkb/rsvps?key=674441542572b783949516b100104c&sign=true&photo-host=public', {
+      method: "POST",
+      body: JSON.stringify({
+        "guests": 0,
+        "response": "no"
+      })
+    });
+    let data = await response.json();
+    return data;
+   } catch(error) {
+    console.error(error);
+  }
+}
+
 class Meetup extends React.Component {
   constructor() {
     super();
     this.state = {
-      meetupJson: {}
+      meetupJson: {},
+      meetupRSVP: {}
     }
   }
 
   componentDidMount() {
     getMeetup().then((list) => {
       this.setState({meetupJson:list});
+    });
+  }
+
+  handleRSVPClick() {
+    postRSVP().then((list) => {
+      this.setState({meetupRSVP:list});
     });
   }
 
@@ -45,7 +68,15 @@ class Meetup extends React.Component {
         minutes = "0" + minutes;
       }
       var name = this.state.meetupJson["0"]["venue"]["name"];
-      return (<p>The next scheduled meetup will be at {hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year} at {name}.</p>);
+      return (
+        <div>
+          <p>The next scheduled meetup will be at {hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year} at {name}.</p>
+          <form action="https://secure.meetup.com/oauth2/access/client_id=ovcnv9ha9jar32damrf4nflcot&redirect_uri=http://localhost:3000/index">
+            <input type="submit" value="Log in to Meetup" />
+          </form>
+          <button onClick={this.handleRSVPClick}>RSVP</button>
+        </div>
+      );
     }
     return <p></p>;
   }
