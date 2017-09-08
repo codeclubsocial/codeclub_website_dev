bef081899b35a96bf4d72eb0a1d7a552a31c5433
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-08 10:48:51 -0700

    test meetup auth using oauth2-client-js

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -12,6 +12,26 @@ async function getMeetup() {
   }
 }
 
+// async function requestToken() {
+//   try {
+//     let response = await fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/oauth/request/', {
+//       method: "POST",
+//       body: JSON.stringify({
+//         oauth_consumer_key: "kksoj0htpfk9ef9c5qcphj0glv",
+//         oauth_signature_method: "PLAINTEXT",
+//         oauth_signature: The signature as defined in Signing Requests.
+//         oauth_timestamp: As defined in Nonce and Timestamp.
+//         oauth_nonce: As defined in Nonce and Timestamp.
+//         oauth_callback:
+//       })
+//     });
+//     let data = await response.json();
+//     return data;
+//    } catch(error) {
+//     console.error(error);
+//   }
+// }
+
 async function postRSVP(rsvpData) {
   try {
     let response = await fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/codeclub/events/kzxzpnywmbkb/rsvps?key=674441542572b783949516b100104c&sign=true&photo-host=public', {
@@ -49,6 +69,34 @@ class Meetup extends React.Component {
     });
   }
 
+  handleLogInClick() {
+    // requestToken().then((list) => {
+    //   this.setState({meetupToken:list});
+    // });
+    var OAuth = require('@zalando/oauth2-client-js');
+    var meetup = new OAuth.Provider({
+      id: 'meetup',   // required
+      authorization_url: 'https://api.meetup.com/oauth/request/' // required
+    });
+    // Create a new request
+    var request = new OAuth.Request({
+        client_id: 'kksoj0htpfk9ef9c5qcphj0glv',  // required
+        redirect_uri: 'http://austinsandbox.herokuapp.com/index'
+    });
+
+    // Give it to the provider
+    var uri = meetup.requestToken(request);
+
+    // Later we need to check if the response was expected
+    // so save the request
+    meetup.remember(request);
+
+    // Do the redirect
+    window.location.href = uri;
+
+    var response = meetup.parse(window.location.hash);
+  }
+
   render() {
     if (Object.keys(this.state.meetupJson).length !== 0) {
       var date = new Date(this.state.meetupJson["0"]["time"]);
@@ -71,9 +119,10 @@ class Meetup extends React.Component {
       return (
         <div>
           <p>The next scheduled meetup will be at {hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year} at {name}.</p>
-          <form action="https://secure.meetup.com/oauth2/authorize?client_id=kksoj0htpfk9ef9c5qcphj0glv&response_type=code&redirect_uri=http://austinsandbox.herokuapp.com/index">
+          {/* <form action="https://secure.meetup.com/oauth2/authorize?client_id=kksoj0htpfk9ef9c5qcphj0glv&response_type=token&redirect_uri=http://austinsandbox.herokuapp.com/index">
             <input type="submit" value="Log in to Meetup" />
-          </form>
+          </form> */}
+          <button onClick={this.handleLogInClick}>Log in to Meetup</button>
           <button onClick={this.handleRSVPClick}>RSVP</button>
         </div>
       );
