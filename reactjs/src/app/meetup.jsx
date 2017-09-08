d1102ce01cf8261371afe33c20bc303d65627a95
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-08 14:06:59 -0700

    working on rsvp state

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -76,8 +76,8 @@ class Meetup extends React.Component {
     var fragments = window.location.hash.split(/&|=/)
     var access_token = fragments[1];
     console.log(fragments[9]);
-    if (fragments[7] == this.urlState) {
-      postRSVP(eventID, access_token, this.state.urlState).then((list) => {
+    if (fragments[7] == this.state.urlState) {
+      postRSVP(eventID, access_token).then((list) => {
         this.setState({meetupRSVP:list});
       });
     }
@@ -130,7 +130,7 @@ class Meetup extends React.Component {
         minutes = "0" + minutes;
       }
       var name = this.state.meetupJson["0"]["venue"]["name"];
-      var hrefAuth = "https://secure.meetup.com/oauth2/authorize?response_type=token&scope=rsvp&client_id=kksoj0htpfk9ef9c5qcphj0glv&redirect_uri=http://austinsandbox.herokuapp.com/index&state=" + this.urlState;
+      var hrefAuth = "https://secure.meetup.com/oauth2/authorize?response_type=token&scope=rsvp&client_id=kksoj0htpfk9ef9c5qcphj0glv&redirect_uri=http://austinsandbox.herokuapp.com/index&state=" + this.state.urlState;
       return (
         <div>
           <p>The next scheduled meetup will be at {hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year} at {name}.</p>
