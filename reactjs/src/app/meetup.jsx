04526f9f0d3784bcfc38081b1e662c6c27cc6e91
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-12 21:53:44 -0700

    made 3 cards, working on making sure each of the 3 RSVP buttons work

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -70,17 +70,17 @@ class Meetup extends React.Component {
   componentDidMount() {
     getMeetup().then((list) => {
       this.setState({meetupJson:list});
-      if (window.location.hash.length > 1) {
-        this.handleRSVPClick();
-      }
     });
+    if (window.location.hash.length > 1) {
+      doRSVP
+    }
   }
 
-  handleRSVPClick() {
-    var eventID = this.state.meetupJson["0"]["id"];
+  doRSVP(eventNum) {
+    var eventID = this.state.meetupJson[eventNum]["id"];
     var fragments = window.location.hash.split(/&|=/)
     var access_token = fragments[1];
-    var cookieState = document.cookie.split(/(urlStateCookie=)|;/);
+    var cookieState = document.cookie.split(/(urlStateCookie=)|;|(eventNum=)/);
     if (fragments[9] == cookieState[2]) {
       postRSVP(eventID, access_token).then((list) => {
         this.setState({meetupRSVP:list});
@@ -91,8 +91,18 @@ class Meetup extends React.Component {
     });
   }
 
+  handleRSVPClick(eventNum) {
+    if (window.location.hash.length <= 1) {
+      this.onLogIn(eventNum);
+      return "https://secure.meetup.com/oauth2/authorize?response_type=token&scope=rsvp&client_id=" + consumerKey + "&redirect_uri=" + redirectURI + "&state=" + this.state.urlState;
+    }
+    else {
+      this.doRSVP(eventNum);
+    }
+  }
+
 // Stores randomly generated state in cookie to be checked when user comes back from meetup auth site
-  onLogIn() {
+  onLogIn(eventNum) {
     var d = new Date();
     // number of days until cookie expires
     var ndays = 1;
@@ -100,52 +110,66 @@ class Meetup extends React.Component {
     var expires = "expires="+ d.toUTCString();
     var newCookie = "urlStateCookie=" + this.state.urlState + ";" + expires + ";path=/";
     document.cookie = newCookie;
+    document.cookie += "eventNum=" + eventNum + ";" + expires + ";path=/"
   }
 
   render() {
     if (Object.keys(this.state.meetupJson).length !== 0) {
-      var date = new Date(this.state.meetupJson["0"]["time"]);
-      var year = 1900 + date.getYear();
-      var month = date.getMonth();
-      var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      var day = date.getDate();
-      var dayXX = ["st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "st"];
-      var hours = date.getHours();
-      var amPm = "AM";
-      if (hours > 12) {
-        hours = hours - 12;
-        amPm = "PM";
-      }
-      var minutes = date.getMinutes();
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      var name = this.state.meetupJson["0"]["venue"]["name"];
-      var hrefAuth = "https://secure.meetup.com/oauth2/authorize?response_type=token&scope=rsvp&client_id=" + consumerKey + "&redirect_uri=" + redirectURI + "&state=" + this.state.urlState;
       var cardStyle = {
         width: "20rem",
       };
-      var finalJSX = [
-        <div>
-          <h4 className="card-title">{this.state.meetupJson["0"]["name"]}</h4>
-          <p className="card-text">{hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year}<span><br/></span>{name}</p>
-          <a href={hrefAuth} onClick={this.onLogIn} className="button card-link">RSVP</a>
-        </div>
-      ];
-      if (Object.keys(this.state.meetupRSVP).length !== 0 && Object.keys(this.state.getMeetupRSVP).length !== 0) {
-        var rsvpList = [];
-        for (var k in this.state.getMeetupRSVP) {
-          rsvpList.push(this.state.getMeetupRSVP[k]["member"]["id"]);
+      var multiCardJSX = [];
+      for (var i = 0; i < 3; i++) {
+        var finalJSX = [];
+        var date = new Date(this.state.meetupJson[i]["time"]);
+        var year = 1900 + date.getYear();
+        var month = date.getMonth();
+        var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var day = date.getDate();
+        var dayXX = ["st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "st"];
+        var hours = date.getHours();
+        var amPm = "AM";
+        if (hours > 12) {
+          hours = hours - 12;
+          amPm = "PM";
         }
-        if (rsvpList.includes(this.state.meetupRSVP["member"]["id"])) {
-          finalJSX.push(<p className="card-text"><span><br/></span>You RSVP'd!</p>);
+        var minutes = date.getMinutes();
+        if (minutes < 10) {
+          minutes = "0" + minutes;
         }
-      }
-      return (
-        <div className="card" style={cardStyle}>
-          <div className="card-body">
-            {finalJSX}
+        var name = this.state.meetupJson[i]["venue"]["name"];
+        finalJSX.push(
+          <div>
+            <h4 className="card-title">{this.state.meetupJson[i]["name"]}</h4>
+            <p className="card-text">{hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year}<span><br/></span>{name}</p>
+            <a href={this.handleRSVPClick(i)} onClick={this.onLogIn} className="button card-link">RSVP</a>
           </div>
+        );
+        if (Object.keys(this.state.meetupRSVP).length !== 0 && Object.keys(this.state.getMeetupRSVP).length !== 0) {
+          var rsvpList = [];
+          for (var k in this.state.getMeetupRSVP) {
+            rsvpList.push(this.state.getMeetupRSVP[k]["member"]["id"]);
+          }
+          if (rsvpList.includes(this.state.meetupRSVP["member"]["id"])) {
+            finalJSX.push(<p className="card-text"><span><br/></span>You RSVP'd!</p>);
+          }
+        }
+        multiCardJSX.push(
+          <div>
+            <div className="card" style={cardStyle}>
+              <div className="card-body">
+                {finalJSX}
+              </div>
+            </div>
+            <span><br/></span>
+          </div>
+        );
+      }
+
+
+      return (
+        <div>
+          {multiCardJSX}
         </div>
       );
     }
