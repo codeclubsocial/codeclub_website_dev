f304148092f276249c16e9703cad0321ef18bffb
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-13 23:19:41 -0700

    working on infinite API call issue

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -154,7 +154,7 @@ class Meetup extends React.Component {
           <div>
             <h4 className="card-title">{this.state.meetupJson[i]["name"]}</h4>
             <p className="card-text">{hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year}<span><br/></span>{name}</p>
-            <a href={this.handleRSVPClick(i)} onClick={() => this.handleLoggedInRSVPClick(i)} className="button card-link">RSVP</a>
+            <a href={this.handleRSVPClick(i)} onClick={(i) => this.handleLoggedInRSVPClick(i)} className="button card-link">RSVP</a>
           </div>
         );
         if (Object.keys(this.state.meetupRSVP).length !== 0 && Object.keys(this.state.getMeetupRSVP).length !== 0) {
