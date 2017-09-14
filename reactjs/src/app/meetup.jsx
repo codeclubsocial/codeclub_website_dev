cedbd5f74ab9bf01bf3e3f2473680e86312eb292
Austin Tackaberry <austintackaberry@gmail.com>
2017-09-13 22:24:25 -0700

    working on infinite API call issue

--- reactjs/src/app/meetup.jsx
+++ reactjs/src/app/meetup.jsx
@@ -74,6 +74,8 @@ class Meetup extends React.Component {
       this.setState({meetupJson:list});
       if (window.location.hash.length > 1) {
         var cookieState = document.cookie.split(/(urlStateCookie=)|;|(eventNum=)/);
+        console.log(cookieState[2]);
+        console.log(cookieState[6]);
         this.doRSVP(cookieState[2], cookieState[6]);
       }
     });
@@ -102,7 +104,7 @@ class Meetup extends React.Component {
     }
   }
 
-  handleLoggedInRSVPClick() {
+  handleLoggedInRSVPClick(eventNum) {
     if (Object.keys(this.state.meetupJson).length !== 0) {
       if (window.location.hash.length <= 1) {
         return true;
@@ -154,7 +156,7 @@ class Meetup extends React.Component {
           <div>
             <h4 className="card-title">{this.state.meetupJson[i]["name"]}</h4>
             <p className="card-text">{hours}:{minutes} {amPm} on {monthList[month]} {day}{dayXX[day-1]}, {year}<span><br/></span>{name}</p>
-            <a href={this.handleRSVPClick(i)} onClick={this.handleLoggedInRSVPClick} className="button card-link">RSVP</a>
+            <a href={this.handleRSVPClick(i)} onClick={this.handleLoggedInRSVPClick(i)} className="button card-link">RSVP</a>
           </div>
         );
         if (Object.keys(this.state.meetupRSVP).length !== 0 && Object.keys(this.state.getMeetupRSVP).length !== 0) {
